1a58f7949b69a4880344427cfb7742c901222ef1
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-01 10:28:04 +0100

    added methods to ScopeLane

--- src/com/danielweisshoff/parser/Parser.java
+++ src/com/danielweisshoff/parser/Parser.java
@@ -2,6 +2,7 @@ package com.danielweisshoff.parser;
 
 import com.danielweisshoff.lexer.Token;
 import com.danielweisshoff.lexer.TokenType;
+import com.danielweisshoff.parser.builders.CallBuilder;
 import com.danielweisshoff.parser.builders.ClassBuilder;
 import com.danielweisshoff.parser.builders.FunctionBuilder;
 import com.danielweisshoff.parser.builders.VariableBuilder;
@@ -10,8 +11,8 @@ import com.danielweisshoff.parser.container.Function;
 import com.danielweisshoff.parser.container.Program;
 import com.danielweisshoff.parser.container.Variable;
 import com.danielweisshoff.parser.nodesystem.Data;
+import com.danielweisshoff.parser.nodesystem.node.CallNode;
 import com.danielweisshoff.parser.nodesystem.node.EntryNode;
-import com.danielweisshoff.parser.nodesystem.node.Node;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,6 +29,7 @@ public class Parser {
     public static HashMap<String, Data<?>> variables = new HashMap<>();
     //Später in MethodGroup umändern
     public static HashMap<String, Function> methods = new HashMap<>();
+
     private final Token[] tokens;
     private final List<Class> classes = new ArrayList<>();
     private final List<EntryNode> entries = new ArrayList<>();
@@ -36,7 +38,7 @@ public class Parser {
     private int position = -1;
 
     public Class currentClass = null;
-    public Node currentFunction = null;
+    public EntryNode currentFunction = null;
 
     public Parser(Token[] tokens) {
         this.tokens = tokens;
@@ -72,9 +74,18 @@ public class Parser {
                 nextLine();
             else if (currentToken.type() == TokenType.TAB) {
                 switch (currentToken.getValue()) {
-                    case "1" -> validateAttributeLane();
-                    case "2" -> validateMethodLane();
-                    default -> validateScopeLane();
+                    case "1" -> {
+                        advance();
+                        validateAttributeLane();
+                    }
+                    case "2" -> {
+                        advance();
+                        validateMethodLane();
+                    }
+                    default -> {
+                        advance();
+                        validateScopeLane();
+                    }
                 }
             } else {
                 validateClassLane();
@@ -90,9 +101,12 @@ public class Parser {
     public boolean compareNextTokens(TokenType... type) {
         for (TokenType t : type) {
             advance();
-            if (currentToken.type() != t)
+            if (currentToken.type() != t) {
+                advance();
                 return false;
+            }
         }
+        advance();
         return true;
     }
 
@@ -111,38 +125,32 @@ public class Parser {
     }
 
     public void validateAttributeLane() {
-        advance();
         if (currentToken.type() == TokenType.KEYWORD) {
             advance();
             Variable v = VariableBuilder.initializeVariable(this);
+            variables.put(v.getName(), v.getData());
         } else
             VariableBuilder.assignVariable(this);
+        nextLine();
     }
 
     public void validateMethodLane() {
-        advance();
         currentFunction = FunctionBuilder.buildFunction(this);
         nextLine();
     }
 
     public void validateScopeLane() {
-        advance();
         if (currentToken.type() == TokenType.KEYWORD) {
-            switch (currentToken.type()) {
-                case NUMBER, FLOAT -> {
-                    advance();
-                    VariableBuilder.initializeVariable(this);
-                    nextLine();
-                }
-            }
-        } else if (next().type() == TokenType.O_ROUND_BRACKET) {
-            //Eine Methode aus dem standard package wird aufgerufen
+            advance();
+            Variable v = VariableBuilder.initializeVariable(this);
+            variables.put(v.getName(), v.getData());
             nextLine();
-        } else if (next().type() == TokenType.DOT) {
-            //Eine Methode einer Klasse/eines Objektes wird aufgerufen
+        } else if (currentToken.type() == TokenType.IDENTIFIER && next().type() == TokenType.ASSIGN) {
+            VariableBuilder.assignVariable(this);
             nextLine();
         } else {
-            VariableBuilder.assignVariable(this);
+            CallNode n = CallBuilder.buildCall(this);
+            currentFunction.add(n);
             nextLine();
         }
     }
