17dded09e330f9eb26165cb1e5d820b3b7a638e3
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-01 13:32:22 +0100

    Formatting

--- src/com/danielweisshoff/interpreter/builtin/BuiltInFunction.java
+++ src/com/danielweisshoff/interpreter/builtin/BuiltInFunction.java
@@ -8,10 +8,10 @@ public abstract class BuiltInFunction {
 
     public static HashMap<String, BuiltInFunction> builtInFunctions = new HashMap<>();
 
-    public abstract Data<?> call();
-
     public static void registerAll() {
         builtInFunctions.put("print", new printFunction());
         builtInFunctions.put("help", new helpFunction());
     }
+
+    public abstract Data<?> call();
 }
