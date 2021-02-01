ed93b71293672ee4f85b994947d06542e39c42ba
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-01 11:09:36 +0100

    now able to call built-in functions

--- src/com/danielweisshoff/parser/builders/CallBuilder.java
+++ src/com/danielweisshoff/parser/builders/CallBuilder.java
@@ -1,5 +1,6 @@
 package com.danielweisshoff.parser.builders;
 
+import com.danielweisshoff.interpreter.builtin.BuiltInFunction;
 import com.danielweisshoff.lexer.TokenType;
 import com.danielweisshoff.parser.Error;
 import com.danielweisshoff.parser.Parser;
@@ -10,13 +11,16 @@ public class CallBuilder {
     public static CallNode buildCall(Parser p) {
         String name = p.currentToken.getValue();
 
-        if (!Parser.methods.containsKey(name))
-            new Error("Methode " + name + " existiert nicht");
+        if (!p.compareNextTokens(TokenType.O_ROUND_BRACKET, TokenType.C_ROUND_BRACKET))
+            new Error("Falsche Funktionsstruktur");
 
-        if (p.compareNextTokens(TokenType.O_ROUND_BRACKET, TokenType.C_ROUND_BRACKET))
-            System.out.println("Methode " + name + " wird aufgerufen");
+        if (!BuiltInFunction.builtInFunctions.containsKey(name))
+            if (!Parser.methods.containsKey(name))
+                new Error("Methode " + name + " existiert nicht");
+
+        System.out.println("Funktionaufruf " + name + " wurde erkannt");
 
         CallNode call = new CallNode(name);
-        return null;
+        return call;
     }
 }
--- src/com/danielweisshoff/parser/nodesystem/node/CallNode.java
+++ src/com/danielweisshoff/parser/nodesystem/node/CallNode.java
@@ -1,5 +1,6 @@
 package com.danielweisshoff.parser.nodesystem.node;
 
+import com.danielweisshoff.interpreter.builtin.BuiltInFunction;
 import com.danielweisshoff.parser.Parser;
 import com.danielweisshoff.parser.nodesystem.Data;
 import com.danielweisshoff.parser.nodesystem.DataType;
@@ -15,9 +16,15 @@ public class CallNode extends Node {
     @Override
     public Data<?> execute() {
         Data<?> data = new Data<>(null, DataType.NULL);
-        if (Parser.methods.containsKey(name)) {
+
+        if (BuiltInFunction.builtInFunctions.containsKey(name)) {
+            data = BuiltInFunction.builtInFunctions.get(name).call();
+            System.out.println("BuiltInFunktion " + name + " wird aufgerufen");
+        } else if (Parser.methods.containsKey(name)) {
             data = Parser.methods.get(name).getNode().execute();
+            System.out.println("Funktion " + name + " wird aufgerufen");
         }
+
         return data;
     }
 }
