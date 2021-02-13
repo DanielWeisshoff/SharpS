077efb9fb2929e9cc0368a56ff6f5850897a3886
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-13 18:33:10 +0100

    Split BinaryOperatorNode

---
+++ src/com/danielweisshoff/interpreter/nodesystem/node/BinarySubNode.java
@@ -0,0 +1,35 @@
+package com.danielweisshoff.interpreter.nodesystem.node;
+
+import com.danielweisshoff.interpreter.nodesystem.Data;
+import com.danielweisshoff.interpreter.nodesystem.DataType;
+
+/**
+ * Subtracts two values and returns the result
+ */
+public class BinarySubNode extends Node {
+
+    private final Node left;
+    private final Node right;
+    private Data<Double> result;
+
+    public BinarySubNode(Node leftNode, Node rightNode) {
+        super(new DataType[]{DataType.DOUBLE, DataType.DOUBLE}, DataType.DOUBLE);
+        this.left = leftNode;
+        this.right = rightNode;
+    }
+
+    @Override
+    public Data<Double> execute() {
+        if (result == null)
+            calculateResult();
+        System.out.println(result);
+        return result;
+    }
+
+    private void calculateResult() {
+        double leftResult = left.execute().toDouble();
+        double rightResult = right.execute().toDouble();
+
+        result = new Data<Double>(leftResult - rightResult, DataType.DOUBLE);
+    }
+}
