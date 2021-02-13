4cd9e3503f5cb4938a49356ff298f1dc2fe4d26d
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-13 18:54:44 +0100

    Split EquationNode.java

---
+++ src/com/danielweisshoff/interpreter/nodesystem/node/logic/NotEqualNode.java
@@ -0,0 +1,32 @@
+package com.danielweisshoff.interpreter.nodesystem.node.logic;
+
+import com.danielweisshoff.interpreter.nodesystem.Data;
+import com.danielweisshoff.interpreter.nodesystem.DataType;
+import com.danielweisshoff.interpreter.nodesystem.node.Node;
+
+/**
+ * Compares the given Nodes
+ * returns 1 or 0
+ */
+public class NotEqualNode extends Node {
+
+    private final Node left;
+    private final Node right;
+
+    public NotEqualNode(Node left, Node right) {
+        super(new DataType[]{DataType.ANY}, DataType.BOOL);
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public Data<Integer> execute() {
+        double a = left.execute().getData().doubleValue();
+        double b = right.execute().getData().doubleValue();
+        boolean bool = a != b;
+
+        if (bool)
+            return new Data<>(1, DataType.INT);
+        return new Data<>(0, DataType.INT);
+    }
+}
