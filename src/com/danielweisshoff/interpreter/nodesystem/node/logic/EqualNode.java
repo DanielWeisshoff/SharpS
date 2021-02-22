1008916009daafd4832be887d348efd2e0728b65
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-22 12:10:23 +0100

    split comparetypes

--- src/com/danielweisshoff/interpreter/nodesystem/node/logic/EqualNode.java
+++ src/com/danielweisshoff/interpreter/nodesystem/node/logic/EqualNode.java
@@ -1,27 +1,21 @@
-package com.danielweisshoff.interpreter.nodesystem.node;
+package com.danielweisshoff.interpreter.nodesystem.node.logic;
 
 import com.danielweisshoff.interpreter.nodesystem.Data;
 import com.danielweisshoff.interpreter.nodesystem.DataType;
-
-/* TODO
- * - In einzelne Nodes auslagern
- */
+import com.danielweisshoff.interpreter.nodesystem.node.Node;
 
 /**
  * Compares the given Nodes
  * returns 1 or 0
  */
-public class EquationNode extends Node {
+public class EqualNode extends Node {
 
     private final Node left;
-    private final String compareType;
     private final Node right;
 
-
-    public EquationNode(Node left, String compareType, Node right) {
+    public EqualNode(Node left, Node right) {
         super(new DataType[]{DataType.ANY}, DataType.BOOL);
         this.left = left;
-        this.compareType = compareType;
         this.right = right;
     }
 
@@ -29,15 +23,8 @@ public class EquationNode extends Node {
     public Data<Integer> execute() {
         double a = left.execute().getData().doubleValue();
         double b = right.execute().getData().doubleValue();
-        boolean bool = false;
-        switch (compareType) {
-            case "<" -> bool = a < b;
-            case "<=" -> bool = a <= b;
-            case ">" -> bool = a > b;
-            case ">=" -> bool = a >= b;
-            case "==" -> bool = a == b;
-            case "!=" -> bool = a != b;
-        }
+        boolean bool = a == b;
+
         if (bool)
             return new Data<>(1, DataType.INT);
         return new Data<>(0, DataType.INT);
