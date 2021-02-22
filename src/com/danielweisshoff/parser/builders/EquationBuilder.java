7d4ca3c91296420b70ea940e834140a78ebdaa17
Daniel Weißhoff <danielwh84@gmail.com>
2021-02-22 12:08:16 +0100

    split compare types

--- src/com/danielweisshoff/parser/builders/EquationBuilder.java
+++ src/com/danielweisshoff/parser/builders/EquationBuilder.java
@@ -1,7 +1,7 @@
 package com.danielweisshoff.parser.builders;
 
-import com.danielweisshoff.interpreter.nodesystem.node.EquationNode;
 import com.danielweisshoff.interpreter.nodesystem.node.Node;
+import com.danielweisshoff.interpreter.nodesystem.node.logic.*;
 import com.danielweisshoff.lexer.Token;
 import com.danielweisshoff.logger.Logger;
 import com.danielweisshoff.parser.Parser;
@@ -13,6 +13,15 @@ public class EquationBuilder {
         p.advance();
         Node rightExpression = ExpressionBuilder.buildExpression(p);
         Logger.log("Gleichung erstellt");
-        return new EquationNode(leftExpression, compareType.getValue(), rightExpression);
+
+        return switch (compareType.getValue()) {
+            case "<" -> new LessNode(leftExpression, rightExpression);
+            case "<=" -> new LessEqualNode(leftExpression, rightExpression);
+            case ">" -> new MoreNode(leftExpression, rightExpression);
+            case ">=" -> new MoreEqualNode(leftExpression, rightExpression);
+            case "==" -> new EqualNode(leftExpression, rightExpression);
+            case "!=" -> new NotEqualNode(leftExpression, rightExpression);
+            default -> null;
+        };
     }
 }
