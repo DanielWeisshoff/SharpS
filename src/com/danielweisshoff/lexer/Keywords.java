3dffc106a4e1193c03c1e8b074d5398e08736bf2
DanielWeisshoff <danielwh84@gmail.com>
2022-03-28 15:03:13 +0200

    rem Keywords.java

--- src/com/danielweisshoff/lexer/Keywords.java
+++
@@ -1,6 +0,0 @@
-package com.danielweisshoff.lexer;
-
-public class Keywords {
-
-	public static final String[] keywords = new String[] { "int", "if", "else", "elif", "fnc", "while", "for", "do" };
-}
